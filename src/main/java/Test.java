f1d0093531f110d43b271d71bdb562891297ef1f
Sam Smith <viswa.inf88@gmail.com>
2018-10-07 01:23:20 +0530

    asasds

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,12 @@ public class Test
     public static void main(String srgs[])
     {
         System.out.println("Test1");
-        int a;
+        int a=10;
+        System.out.println(a);
+        int b=20;
+        System.out.println(b);
+        int b=20;
+        System.out.println(b);
+
     }
 }
